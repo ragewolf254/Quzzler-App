5fb970cfa7aac3110c64154e10a20ebfca6be0cc
ragewolf254 <ragewolf254@yahoo.com>
2018-01-10 13:59:10 +0800

    Added a new class Added constructors for boolean operations Added a scratch paper for clarification Edited code for understanding Fixed orientation issues: Set the orientation to portrait only Added an alert dialog for displaying an exit prompt Added two functions for checking the answers and updating the score Shortened code and replaced the long method of setting the answer into a shorter one

--- app/src/main/java/com/ragew/quizzler/MainActivity.java
+++ app/src/main/java/com/ragew/quizzler/MainActivity.java
@@ -1,23 +1,32 @@
 package com.ragew.quizzler;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
     // TODO: Declare constants here
+
+    // TODO: Declare member variables here:
     Button m_TrueButton;
     Button m_FalseButton;
-    TextView m_textView;
-    // TODO: Declare member variables here:
+    TextView m_questionView;
+    TextView m_scoreView;
+    ProgressBar m_progressBar;
+
     int m_index; // get the index of the questions
     int m_question;
+    int m_score;
+    boolean m_userAnswer;
     // TODO: Uncomment to create question bank
     private TrueFalse[] mQuestionBank = new TrueFalse[] //initialized as new TrueFalse
     {
@@ -36,6 +45,8 @@ public class MainActivity extends Activity {
             new TrueFalse(R.string.question_13,true)
     };
 
+    final int PROGRESS_BAR_INCREMENT = (int) Math.ceil(100.0/mQuestionBank.length);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,16 +54,21 @@ public class MainActivity extends Activity {
 
         m_TrueButton = (Button) findViewById(R.id.true_button);
         m_FalseButton = (Button) findViewById(R.id.false_button);
-        m_textView = (TextView) findViewById(R.id.question_text_view);
+        m_questionView = (TextView) findViewById(R.id.question_text_view);
 
         m_question = mQuestionBank[m_index].getQuestionID();
-        m_textView.setText(m_question);
+        m_questionView.setText(m_question);
+
+        m_scoreView = (TextView) findViewById(R.id.score);
+
+        m_progressBar = (ProgressBar) findViewById(R.id.progress_bar);
 
         m_TrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.d("Quizzap", "True has been pressed");
-                Toast.makeText(getApplicationContext(),"True button has been pressed!", Toast.LENGTH_LONG).show();
+                checkAnswer(true);
+                updateQuestion();
             }
         });
 
@@ -60,10 +76,44 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
                 Log.d("Quizapp", "False has been pressed");
-                Toast.makeText(getApplicationContext(), "False button has been pressed!", Toast.LENGTH_LONG).show();
+                checkAnswer(false);
+                updateQuestion();
             }
         });
     }
 
+    private void updateQuestion(){
+        m_index = (m_index + 1) % mQuestionBank.length;
+        m_question = mQuestionBank[m_index].getQuestionID();
+        m_questionView.setText(m_question);
+        if (m_index == 0){
+            AlertDialog.Builder m_alert = new AlertDialog.Builder(this);
+            m_alert.setTitle("Game Over!");
+            m_alert.setMessage("Your score is: " + m_score + ".");
+            m_alert.setCancelable(false);
+            m_alert.setPositiveButton("Close Application", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    finish();
+                }
+            });
+            m_alert.show();
+//            Toast.makeText(getApplicationContext(),"Your score is: " + m_score + ". Goodbye!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void checkAnswer(boolean userSelection){
+        boolean correctAnswer = mQuestionBank[m_index].isTrueFalse();
+        if (userSelection == correctAnswer ){
+            Toast.makeText(getApplicationContext(), R.string.correct_toast, Toast.LENGTH_LONG).show();
+            m_score = m_score + 1;
+            m_scoreView.setText("Score " + m_score + "/" + mQuestionBank.length);
+            m_progressBar.incrementProgressBy(PROGRESS_BAR_INCREMENT);
+            
+        } else {
+            Toast.makeText(getApplicationContext(), R.string.incorrect_toast, Toast.LENGTH_LONG).show();
+            m_progressBar.incrementProgressBy(PROGRESS_BAR_INCREMENT);
+        }
+    }
 
 }
